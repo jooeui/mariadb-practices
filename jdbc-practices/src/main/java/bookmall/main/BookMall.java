a1c4ae685ec92978c8d42d6f970761d69dbd3ba9
jooeui <kje_0727@naver.com>
2021-10-06 16:07:28 +0900

    21-10-06 DAO 패턴 #final

---
+++ jdbc-practices/src/main/java/bookmall/main/BookMall.java
@@ -0,0 +1,15 @@
+package bookmall.main;
+
+public class BookMall {
+
+	public static void main(String[] args) {
+		new CategoryDao().insert(vo);
+		new CategoryDao().insert(vo);
+		new CategoryDao().insert(vo);
+		
+		new CategoryDao().findAll();
+		
+		System.out.println("========== 회원 리스트 ==========");
+	}
+
+}
--- jdbc-practices/src/main/java/bookshop/dao/BookDao.java
+++ jdbc-practices/src/main/java/bookshop/dao/BookDao.java
@@ -3,7 +3,10 @@ package bookshop.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import bookshop.vo.BookVo;
 
@@ -49,6 +52,98 @@ public class BookDao {
 		return result;
 	}
 	
+	public List<BookVo> findAll() {
+		List<BookVo> result = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			// 3. SQL 준비
+			String sql = "select b.no, b.title, b.status, a.name as author_name "
+					+ " from author a, book b "
+					+ " where a.no = b.author_no "
+					+ " order by b.no desc";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. 바인딩(binding)
+			
+			// 5. SQL 실행
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String title = rs.getString(2);
+				String status = rs.getString(3);
+				String authorName = rs.getString(4);
+				
+				BookVo vo = new BookVo();
+				vo.setNo(no);
+				vo.setTitle(title);
+				vo.setStatus(status);
+				vo.setAuthorName(authorName);
+				
+				result.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			// clean up
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+
+	public boolean update(Long no, String status) {
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			
+			// 3. SQL 준비
+			String sql = "update book set status = ? where no = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. 바인딩(binding)
+			pstmt.setString(1, status);
+			pstmt.setLong(2, no);
+			
+			// 5. SQL 실행
+			int count = pstmt.executeUpdate();
+			
+			result = count == 1;
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} finally {
+			// clean up
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 	
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
@@ -67,4 +162,5 @@ public class BookDao {
 
 		return conn;
 	}
+
 }
