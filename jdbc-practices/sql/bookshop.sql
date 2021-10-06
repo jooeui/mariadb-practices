71ee7c60e8d6d9fdfaed055d413e36357a511e2f
jooeui <kje_0727@naver.com>
2021-10-06 15:19:21 +0900

    21-10-06 DAO 패턴 #5

--- jdbc-practices/sql/bookshop.sql
+++ jdbc-practices/sql/bookshop.sql
@@ -4,7 +4,7 @@ desc author;
 
 -- insert author
 insert into author values (null, '스테파니메이어');
-select * from author;
+select no, name from author;
 
 -- insert book
 insert into book values(null, '이클립스', 1, '대여가능');
@@ -13,6 +13,12 @@ select * from book;
 -- select book
 select b.no, b.title, b.status, a.name as author_name
 from author a, book b
-where a.no = book.author_no;
+where a.no = b.author_no
+order by b.no desc;
 
 -- update book's status(no, status)
+update book
+set status = '대여중'
+where no = 1;
+
+select * from book;
