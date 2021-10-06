cf4ebdaa821a2a3e474e98ac6825c3e0c83735a3
jooeui <kje_0727@naver.com>
2021-10-06 18:17:43 +0900

    practice04 끝!!!!!!!!!!!!!!

--- sql-practices/연습문제/practice04.sql
+++ sql-practices/연습문제/practice04.sql
@@ -74,12 +74,14 @@ where e.emp_no = s.emp_no
 -- 문제4.
 -- 현재, 사원들의 사번, 이름, 매니저 이름, 부서 이름으로 출력해 보세요.
 -- 1) 매니저 이름 출력
-select dm.dept_no, concat(e.first_name, ' ', e.last_name) as manager_name
-from dept_manager dm, employees e
+select dm.dept_no, d.dept_name, concat(e.first_name, ' ', e.last_name) as manager_name
+from dept_manager dm, employees e, departments d
 where dm.emp_no = e.emp_no
+	and dm.dept_no = d.dept_no
 	and dm.to_date = '9999-01-01';
 
-select *
+-- 2) 사원 정보 및 매니저 이름
+select e.emp_no as '사번', concat(e.first_name, ' ', e.last_name) as '사원 이름', dm.manager_name as '매니저 이름', d.dept_name as '부서 이름'
 from employees e,
 	 dept_emp de,
 	 departments d,
@@ -95,19 +97,179 @@ where e.emp_no = de.emp_no
 
 -- 문제5.
 -- 현재, 평균연봉이 가장 높은 부서의 사원들의 사번, 이름, 직책, 연봉을 조회하고 연봉 순으로 출력하세요.
+-- 1) 부서별 평균 연봉
+select de.dept_no, avg(s.salary) as avg_salary
+from salaries s, dept_emp de
+where s.emp_no = de.emp_no
+	and s.to_date = '9999-01-01'
+	and de.to_date = '9999-01-01'
+group by de.dept_no
+order by avg(s.salary) desc;
 
+-- 2-1) top-k
+-- 평균연봉이 가장 높은 부서
+select de.dept_no, avg(s.salary) as avg_salary
+from salaries s, dept_emp de
+where s.emp_no = de.emp_no
+	and s.to_date = '9999-01-01'
+	and de.to_date = '9999-01-01'
+group by de.dept_no
+order by avg(s.salary) desc
+limit 1;
+-- 3-1) 평균 연봉이 가장 높은 부서의 사원 정보
+select e.emp_no as '사번', concat(e.first_name, ' ', e.last_name) as '이름', t.title as '직책', s.salary as '연봉'
+from employees e, 
+	 titles t, 
+     salaries s, 
+     dept_emp de,
+     (select de.dept_no, avg(s.salary) as avg_salary
+		from salaries s, dept_emp de
+		where s.emp_no = de.emp_no
+			and s.to_date = '9999-01-01'
+			and de.to_date = '9999-01-01'
+		group by de.dept_no
+		order by avg(s.salary) desc
+		limit 1) sub1
+where e.emp_no = t.emp_no
+	and e.emp_no = s.emp_no
+    and e.emp_no = de.emp_no
+    and de.dept_no = sub1.dept_no
+    and t.to_date = '9999-01-01'
+    and s.to_date = '9999-01-01'
+    and de.to_date = '9999-01-01'
+order by s.salary asc;
+
+-- 2-1) subquery 2개
+-- 평균연봉이 가장 높은 부서
+select ad.dept_no, max(avg_salary)
+from ( select de.dept_no, avg(s.salary) as avg_salary
+		from salaries s, dept_emp de
+		where s.emp_no = de.emp_no
+			and s.to_date = '9999-01-01'
+			and de.to_date = '9999-01-01'
+		group by de.dept_no
+		order by avg(s.salary) desc) ad;
+-- 3-1) 평균 연봉이 가장 높은 부서의 사원 정보
+select e.emp_no as '사번', concat(e.first_name, ' ', e.last_name) as '이름', t.title as '직책', s.salary as '연봉'
+from employees e, 
+	 titles t, 
+     salaries s, 
+     dept_emp de,
+     (select ad.dept_no, max(avg_salary)
+		from ( select de.dept_no, avg(s.salary) as avg_salary
+				from salaries s, dept_emp de
+				where s.emp_no = de.emp_no
+					and s.to_date = '9999-01-01'
+					and de.to_date = '9999-01-01'
+				group by de.dept_no
+				order by avg(s.salary) desc) ad) md
+where e.emp_no = t.emp_no
+	and e.emp_no = s.emp_no
+    and e.emp_no = de.emp_no
+    and de.dept_no = md.dept_no
+    and t.to_date = '9999-01-01'
+    and s.to_date = '9999-01-01'
+    and de.to_date = '9999-01-01'
+order by s.salary asc;
 
 
 -- 문제6.
 -- 평균 연봉이 가장 높은 부서는? 
+-- 풀이1) top-k
+select dept_name as '평균 연봉 가장 높은 부서'
+from departments d,
+	 (select de.dept_no, avg(s.salary) as avg_salary
+		from salaries s, dept_emp de
+		where s.emp_no = de.emp_no
+			and s.to_date = '9999-01-01'
+			and de.to_date = '9999-01-01'
+		group by de.dept_no
+		order by avg(s.salary) desc
+		limit 1) sub1
+where d.dept_no = sub1.dept_no;
 
+-- 풀이2) subquery 2개
+select d.dept_name as '평균 연봉 가장 높은 부서'
+from departments d,
+     (select ad.dept_no, max(avg_salary)
+		from ( select de.dept_no, avg(s.salary) as avg_salary
+				from salaries s, dept_emp de
+				where s.emp_no = de.emp_no
+					and s.to_date = '9999-01-01'
+					and de.to_date = '9999-01-01'
+				group by de.dept_no
+				order by avg(s.salary) desc) ad) md
+where d.dept_no = md.dept_no;
 
 
 -- 문제7.
 -- 평균 연봉이 가장 높은 직책?
+-- 1) 직책별 평균 연봉
+select t.title, avg(s.salary) as avg_salary
+from salaries s, titles t
+where s.emp_no = t.emp_no
+	and s.to_date = '9999-01-01'
+	and t.to_date = '9999-01-01'
+group by t.title
+order by avg_salary desc;
 
+-- 2) 평균 연봉이 가장 높은 직책
+-- 2-1) top-k
+select distinct t.title as '평균 연봉 가장 높은 직책'
+from titles t,
+	 (select t.title, avg(s.salary) as avg_salary
+		from salaries s, titles t
+		where s.emp_no = t.emp_no
+			and s.to_date = '9999-01-01'
+			and t.to_date = '9999-01-01'
+		group by t.title
+		order by avg_salary desc
+		limit 1) sub1
+where t.title = sub1.title;
+
+-- 2-2) subquery 2개
+select distinct t.title as '평균 연봉 가장 높은 직책'
+from titles t,
+     (select t1.title, max(avg_salary)
+		from ( select t.title, avg(s.salary) as avg_salary
+				from salaries s, titles t
+				where s.emp_no = t.emp_no
+					and s.to_date = '9999-01-01'
+					and t.to_date = '9999-01-01'
+				group by t.title
+				order by avg_salary desc) t1) mt
+where t.title = mt.title;
 
 
 -- 문제8.
 -- 현재 자신의 매니저보다 높은 연봉을 받고 있는 직원은?
 -- 부서이름, 사원이름, 연봉, 매니저 이름, 메니저 연봉 순으로 출력합니다.
+-- 1) 매니저 이름, 연봉
+select dm.dept_no, concat(e.first_name, ' ', e.last_name) as manager_name, s.salary
+from dept_manager dm, employees e, salaries s
+where dm.emp_no = e.emp_no
+	and dm.emp_no = s.emp_no
+	and dm.to_date = '9999-01-01'
+	and s.to_date = '9999-01-01';
+    
+-- 2) 매니저보다 연봉이 높은 사원의 정보
+select d.dept_name as '부서이름', concat(e.first_name, ' ', e.last_name) as '사원이름', s.salary as '연봉', 
+	   dms.manager_name as '매니저 이름', dms.salary as '매니저 연봉'
+from dept_emp de,
+	 departments d,
+	 employees e, 
+     salaries s, 
+     (select dm.dept_no, concat(e.first_name, ' ', e.last_name) as manager_name, s.salary
+		from dept_manager dm, employees e, salaries s
+		where dm.emp_no = e.emp_no
+			and dm.emp_no = s.emp_no
+			and dm.to_date = '9999-01-01'
+			and s.to_date = '9999-01-01') dms
+where de.dept_no = d.dept_no
+	and de.emp_no = e.emp_no
+    and e.emp_no = s.emp_no
+    and de.dept_no = dms.dept_no
+    and de.to_date = '9999-01-01'
+    and s.to_date = '9999-01-01'
+    and s.salary > dms.salary
+order by d.dept_name, e.first_name, e.last_name, s.salary;
